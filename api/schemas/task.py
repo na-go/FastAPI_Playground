894415db4a2bcf1fc085a8d66e25627d9c903293
na-go <kn.mbk201.mynk@gmail.com>
2022-07-31 22:44:50 +0900

    12章でバグった

---
+++ api/schemas/task.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+class TaskBase(BaseModel):
+    title: Optional[str] = Field(None, example="クリーニングを取りに行く")
+
+
+class TaskCreate(TaskBase):
+    pass
+
+
+class TaskCreateResponse(TaskCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
